af53a4c5a7d6651f45150a336bd8f15cdcd86f92
Jim Martens <github@2martens.de>
2023-07-22 16:45:43 +0200

    Use table to present data

--- src/app/elections/elections.component.ts
+++ src/app/elections/elections.component.ts
@@ -1,21 +1,28 @@
 import {Component, OnInit} from '@angular/core';
 import {KeycloakService} from "keycloak-angular";
 import {Election} from "./model/election";
-import {Observable} from "rxjs";
+import {tap} from "rxjs";
 import {Store} from "@ngrx/store";
 import {allElections} from "./store";
 import {loadAllElectionsAction} from "./store/elections.actions";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-elections',
   templateUrl: './elections.component.html',
   styleUrls: ['./elections.component.scss']
 })
-export class ElectionsComponent implements OnInit{
-  $elections: Observable<Election[]> = this.store.select<Election[]>(allElections());
+export class ElectionsComponent implements OnInit {
+  electionColumns = ['name', 'day', 'totalNumberOfSeats', 'votingThreshold'];
+  dataSource = new MatTableDataSource<Election>();
+  elections$;
 
   constructor(private keycloakService: KeycloakService,
               private store: Store) {
+    this.elections$ = this.store.select(allElections())
+      .pipe(
+        tap(elections => this.dataSource.data = elections)
+      );
   }
 
   ngOnInit() {
