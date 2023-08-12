76da8c564b038a514667183e5c492904749538d6
Jim Martens <github@2martens.de>
2023-08-12 18:14:08 +0200

    Change backendURL to api.2martens.de

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  backendURL: "https://wahlrecht.2martens.de/wahlrecht/v1",
+  backendURL: "https://api.2martens.de/wahlrecht/v1",
   keycloakURL: "https://id.2martens.de",
   realm: "2martens",
   clientId: "wahlrecht-frontend"
